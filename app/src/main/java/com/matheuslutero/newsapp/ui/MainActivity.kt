c051c99f848afd15dca7e2801df351752fb1bdf3
Matheus Lutero <matheuslutero@outlook.com>
2023-03-27 04:36:14 -0300

    feat: initial project setup (#1)

---
+++ app/src/main/java/com/matheuslutero/newsapp/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.matheuslutero.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.matheuslutero.newsapp.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
